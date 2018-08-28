041dcc18e3ff8ebe2ce47af3fdb0ab08d9efa4e5
AMIR  HASSAN <amirhassan@AMIRs-MacBook-Air.local>
2018-08-27 21:27:58 -0400

    updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,4 @@
 var yourScore = document.getElementById('yourScore');
-//var yourScore = getElementById(red, blue, yellow, green.onclick(math.sum));
 yourScore.innerHTML = 0; //
 var targetScore = document.getElementById('targetScore');
 targetScore.innerHTML = getRandomValue(10, 4);
@@ -11,29 +10,20 @@ function addToScore(val) {
 }
 
 var red = document.getElementById("red");
-//red.setAttribute("value", "5");
 console.log(red.value);
 
 var blue = document.getElementById("blue");
-//console.log(blue.value);
 
 var yellow = document.getElementById("yellow");
-//console.log(yellow.value);
 
 var green = document.getElementById("green");
-//console.log(green.value);
 
-// add an event listener to elements
 red.addEventListener('click', function() {
   //this refers to red
   addToScore(this.value);
   checkPlayerScore();
 });
 
-//parseInt(42.01) => 42
-//alert(parseInt('james')); // NaN (not a number)
-// Cafe Del Mar ChillWave 2 :)
-// .innerHTML returns a string, NaN
 
 blue.addEventListener('click', function() {
   // this refers to blue
@@ -52,7 +42,6 @@ green.addEventListener('click', function() {
   addToScore(this.value);
   checkPlayerScore();
 });
-//var sum = parseInt(red.value) + parseInt(blue.value) + parseInt(yelow.value) + parseInt(green.value);
 
 // console.log (sum);
 function getRandomValue (a, b){
@@ -60,7 +49,6 @@ function getRandomValue (a, b){
 }
 
 function checkPlayerScore() {
-  //yourScore, targetScore (parseFloat() is for decimals)
   var playerInt = parseInt(yourScore.innerHTML);
   var targetInt = parseInt(targetScore.innerHTML);
   if (playerInt === targetInt) {
